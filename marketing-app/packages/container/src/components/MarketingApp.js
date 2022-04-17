74f1a1a04518415683d01b4d59fdec32ffe22537
Miguel Torres <mgltorsa@gmail.com>
2022-04-17 00:04:50 -0500

    added routing fixes to sync container browser router and marketing's in  memory router

--- marketing-app/packages/container/src/components/MarketingApp.js
+++ marketing-app/packages/container/src/components/MarketingApp.js
@@ -1,12 +1,32 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+
 
 export default () => {
   const ref = useRef(null);
 
+  const history = useHistory()
+
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      // onNavigate is being used to sync container's router
+      // with child memory rotuer
+      onNavigate: ({ pathname: nextPathname }) => {
+        // To avoid infinite loop if you're already showing
+        // a pathname and tried to acces it again
+        const { pathname } = history.location
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      }
+    });
+
+    // used to update child's router with
+    // container's router info
+    history.listen(onParentNavigate)
+  }, []);
 
   return <div ref={ref} />;
 };
